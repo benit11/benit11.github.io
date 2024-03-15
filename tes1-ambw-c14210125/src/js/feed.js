0f5d602ab61d45173afd13518d011829b4cdab63
benita <109541073+benit11@users.noreply.github.com>
2024-03-15 10:02:27 +0700

    up

--- tes1-ambw-c14210125/src/js/feed.js
+++ tes1-ambw-c14210125/src/js/feed.js
@@ -1,56 +1,29 @@
 
 function modal_detail(id, desc, img, nama, tutor) {
-   // Set data to the modal elements
-   $("#workoutName").text(nama);
-   $("#workoutDesc").text(desc);
-   $(".modal-image").attr("src", "images/" + img);
-   $(".btn-tutor").attr("href", tutor);
- 
-    // Save data to sessionStorage
-    const workoutData = { id: id, desc: desc, img: img, nama: nama };
-    sessionStorage.setItem(nama, JSON.stringify(workoutData));
-   // Show the modal
-   $("#workoutModal").modal("show");
-  // if (navigator.onLine) {
-  //   // Fetch data from network and show modal
-  //   fetch(url)
-  //     .then(function (res) {
-  //       return res.json();
-  //     })
-  //     .then(function (data) {
-  //       // Set modal data
-  //       $("#workoutName").text(nama);
-  //       $("#workoutDesc").text(desc);
-  //       $(".modal-image").attr("src", "images/" + img);
-  //       $(".btn-tutor").attr("href", tutor);
-
-  //       const workoutData = { id: id, desc: desc, img: img, nama: nama };
-  //       sessionStorage.setItem(nama, JSON.stringify(workoutData));
-
-  //       // Show modal
-  //       $("#workoutModal").modal("show");
-  //     });
-  // } else {
-  //   // Check if data exists in localStorage
-  //   const workoutData = JSON.parse(sessionStorage.getItem(nama));
-  //   if (workoutData) {
-  //     // Set modal data from localStorage
-  //     $("#workoutName").text(workoutData.nama);
-  //     $("#workoutDesc").text(workoutData.desc);
-  //     $(".modal-image").attr("src", "images/" + workoutData.img);
-  //     $(".btn-tutor").attr("href", workoutData.tutor);
-
-  //     // Show modal
-  //     $("#workoutModal").modal("show");
-  //   } else {
-  //     // Modal cannot be opened offline without cached data
-  //     alert("Data tidak tersedia saat offline. Silahkan hubungkan internet Anda untuk membuka modal.");
-  //   }
-  // }
+    if (navigator.onLine) {
+        $("#workoutName").text(nama);
+        $("#workoutDesc").text(desc);
+        $(".modal-image").attr("src", "images/" + img);
+        $(".btn-tutor").attr("href", tutor);
+        $("#workoutModal").modal("show");
+        const workoutData = { id: id, desc: desc, img: img, nama: nama };
+        sessionStorage.setItem(nama, JSON.stringify(workoutData));
+    } else {
+        const workoutData = JSON.parse(sessionStorage.getItem(nama));
+        if (workoutData){
+          $("#workoutName").text(nama);
+          $("#workoutDesc").text(desc);
+          $(".modal-image").attr("src", "images/" + img);
+          $(".btn-tutor").attr("href", tutor);
+            $("#workoutModal").modal("show");
+          $("#workoutModal").modal("show");
+        }else{
+          alert("Data tidak tersedia saat offline. Silahkan hubungkan internet Anda untuk membuka modal.");
+          $("#workoutDesc").text("ANDA SEDANG OFFLINE DATA TIDAK TERSEDIA");
+        }
+    }
 }
 
-
-// Mengubah fungsi cardButton.click pada fungsi createCards
 function createCards() {
   const container = $("#activity");
   const workouts = JSON.parse(localStorage.getItem("workouts"));
@@ -84,29 +57,10 @@ function createCards() {
       color: "white",
     });
 
-    let alertShown = false; // Variabel untuk menunjukkan apakah alert telah ditampilkan atau tidak
 
     cardButton.click(() => {
-      if (navigator.onLine) { // Memastikan data telah diterima dari jaringan
         modal_detail(value.id, value.desc, value.img, value.nama, value.tutor);
-      } else {
-        // console.log(value.nama);
-        const workoutData = JSON.parse(sessionStorage.getItem(value.nama));
-        if (workoutData) {
-          if (!alertShown) { // Memeriksa apakah pesan alert sudah ditampilkan sebelumnya
-            alert("Data tersimpan secara lokal. Silahkan hubungkan internet Anda untuk memperbarui data.");
-            alertShown = true; // Mengatur bahwa pesan alert telah ditampilkan
-          }
-          // Modal tidak dipanggil di sini karena data tersedia secara lokal
-        } else {
-          if (!alertShown) { // Memeriksa apakah pesan alert sudah ditampilkan sebelumnya
-            alert("Data tidak tersedia secara lokal. Silahkan hubungkan internet Anda untuk membuka modal.");
-            alertShown = true; // Mengatur bahwa pesan alert telah ditampilkan
-          }
-          // Modal tidak dipanggil di sini karena data tidak tersedia secara lokal
-        }
-      }
-    });
+        });
 
     cardBody.append( cardImageWrapper, cardButton);
     cardWrapper.append(cardBody);
